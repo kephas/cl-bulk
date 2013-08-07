5ba47531b34f7756437eea4ae7f9673bd6ce60db
Pierre Thierry <pierre@nothos.net>
2013-08-08 00:17:13 +0200

    Add whole stream reading

--- proto.lisp
+++ proto.lisp
@@ -1,11 +1,11 @@
 (in-package :nothos.net/2013.08.bulk)
 
-(defun %read-form-payload (stream)
+(defun %read-form-payload (stream &optional top-level?)
   (let@ rec ((expressions)
-	     (next (read-bulk stream)))
+	     (next (read-bulk stream top-level?)))
     (if (eq next :end)
 	(reverse expressions)
-	(rec (cons next expressions) (read-bulk stream)))))
+	(rec (cons next expressions) (read-bulk stream top-level?)))))
 
 (defun %read-array-payload (stream)
   (let* ((size (read-bulk stream))
@@ -27,12 +27,12 @@
 
 (define-condition parsing-error (error) ())
 
-(defun read-bulk (stream)
-  (let ((marker (read-byte stream)))
+(defun read-bulk (stream &optional top-level?)
+  (let ((marker (read-byte stream (not top-level?) :end)))
     (case marker
       (0 nil)
       (1 (%read-form-payload stream))
-      (2 :end)
+      (2 (if top-level? (error 'parsing-error) :end))
       (3 (%read-array-payload stream))
       (4 (%read-unsigned-payload stream 1))
       (5 (%read-unsigned-payload stream 2))
@@ -41,4 +41,8 @@
       (8 (%read-unsigned-payload stream 16))
       (9 (%read-signed-payload stream))
       ((10 11 12 13 14 15) (error 'parsing-error))
+      (:end :end)
       (t (%read-ref-payload stream marker)))))
+
+(defun read-whole (stream)
+  (%read-form-payload stream t))
